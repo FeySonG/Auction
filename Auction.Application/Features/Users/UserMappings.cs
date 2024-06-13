83f71d3bccd59fd76d2db6cd314003d73e07fca0
Nazar <133972657+QuinqueQ@users.noreply.github.com>
2024-06-13 18:45:03 +0000

    profileMapping

--- Auction.Application/Features/Users/UserMappings.cs
+++ Auction.Application/Features/Users/UserMappings.cs
@@ -7,6 +7,17 @@
             CreateMap<User, UserResponseDTO>().ReverseMap();
             CreateMap<User, UserCreateDTO>().ReverseMap();
             CreateMap<User, UserUpdateDTO>().ReverseMap();
+
+            CreateMap<User, UserGetProfileDTO>()
+           .ForMember(dest => dest.BankCard, opt => opt.MapFrom(src => src.BankCard))
+           .ForMember(dest => dest.Contact, opt => opt.MapFrom(src => src.Contact))
+           .ReverseMap()
+           .ForMember(dest => dest.BankCard, opt => opt.MapFrom(src => src.BankCard))
+           .ForMember(dest => dest.Contact, opt => opt.MapFrom(src => src.Contact));
+
+
+            CreateMap<UserContact, UserContactGetDTO>();
+            CreateMap<PaymentCard, PaymentCardGetDTO>();
         }
     }
 }
